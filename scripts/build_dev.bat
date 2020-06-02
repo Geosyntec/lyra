890a00e4dd4a02ba8d83f05560f842609d10cffd
Austin Orr <AOrr@geosyntec.com>
2020-06-02 12:58:13 -0700

    create docker build pattern

--- scripts/build_dev.bat
+++ scripts/build_dev.bat
@@ -5,6 +5,8 @@ call docker-compose ^
 -f docker-compose.shared.build.yml ^
 -f docker-compose.shared.depends.yml ^
 -f docker-compose.shared.env.yml ^
+-f docker-compose.dev.volumes.yml ^
+-f docker-compose.dev.ports.yml ^
 config > docker-stack.yml
 
 docker-compose -f docker-stack.yml build
