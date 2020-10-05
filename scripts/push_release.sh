c3f5b63dafefa17eeeab9357361c9e1884a2428d
Austin Orr <austinmartinorr@gmail.com>
2020-10-05 09:59:45 -0700

    update release.sh

--- scripts/push_release.sh
+++ scripts/push_release.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-tag=$(grep __version__ ./lyra/lyra/__init__.py | cut -d '=' -f2 | sed 's/"//g' | tr -d '[:space:]')
+# default is to tag the image with the full version.
+vtag=$(grep __version__ ./lyra/lyra/__init__.py | cut -d '=' -f2 | sed 's/"//g' | tr -d '[:space:]')
+
+while getopts t: flag
+do
+    case "${flag}" in
+    	# user override for image tag
+    	# usage:
+    	#~$ push_release.sh -t 1.3.0
+        t) tag=${OPTARG:-vtag};;
+    esac
+done
 
 echo "building tagged release: $tag"
 bash scripts/build_prod.sh -t "$tag"
