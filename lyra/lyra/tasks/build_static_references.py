491cdf8951deb602e8791cc18618b6edf436a6e8
Austin Orr <AOrr@geosyntec.com>
2021-06-01 11:01:07 -0700

    rebuilt static resources

--- lyra/lyra/tasks/build_static_references.py
+++ lyra/lyra/tasks/build_static_references.py
@@ -18,7 +18,20 @@ def get_site_list() -> Dict[str, Any]:
     site_list = {
         "function": "get_site_list",
         "version": 1,
-        "params": {"site_list": "TSFILES(DSOURCES(tscARCHIVE))"},
+        "params": {"site_list": "TABLE(SITE)"},
+    }
+
+    response = asyncio.run(send_request(settings.HYDSTRA_BASE_URL, payload=site_list))
+    result: Dict[str, Any] = response["_return"]
+
+    return result
+
+
+def get_swn_site_list() -> Dict[str, Any]:
+    site_list = {
+        "function": "get_site_list",
+        "version": 1,
+        "params": {"site_list": "GROUP(SITE_TYPE,SWN_ALISO)"},
     }
 
     response = asyncio.run(send_request(settings.HYDSTRA_BASE_URL, payload=site_list))
@@ -43,6 +56,8 @@ def site_preferred_variables(site_list: List[str]) -> pandas.DataFrame:
         send_request(settings.HYDSTRA_BASE_URL, payload=get_variable_list)
     )
 
+    swn_site_list = get_swn_site_list()
+
     vars_by_site = []
 
     for blob in promise["_return"]["sites"]:
@@ -58,17 +73,21 @@ def site_preferred_variables(site_list: List[str]) -> pandas.DataFrame:
         "Discharge": {"agg": "mean"},
     }
 
-    variables = variables.merge(
-        variables.query("name in @use_vars.keys()")
-        .groupby(["site", "name"])["variable"]
-        .min()  # <-- use the minimum number as the 'preferred value for now. Just a WAG.'
-        .reset_index()
-        .assign(preferred=True)
-        .set_index(["site", "name", "variable"])["preferred"],
-        left_on=["site", "name", "variable"],
-        right_index=True,
-        how="left",
-    ).fillna(False)
+    variables = (
+        variables.assign(in_swn=lambda df: df["site"].isin(swn_site_list["sites"]))
+        .merge(
+            variables.query("name in @use_vars.keys()")
+            .groupby(["site", "name"])["variable"]
+            .min()  # <-- use the minimum number as the 'preferred value for now. Just a WAG.'
+            .reset_index()
+            .assign(preferred=True)
+            .set_index(["site", "name", "variable"])["preferred"],
+            left_on=["site", "name", "variable"],
+            right_index=True,
+            how="left",
+        )
+        .fillna(False)
+    )
 
     return variables
 
