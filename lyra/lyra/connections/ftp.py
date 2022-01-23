4a14cad2948d7f0426cdbef37f647da5160d5405
Austin Orr <AOrr@geosyntec.com>
2022-01-23 13:44:37 -0800

    patch FTP secure transfer (TLS support)

--- lyra/lyra/connections/ftp.py
+++ lyra/lyra/connections/ftp.py
@@ -1,8 +1,29 @@
 import ftplib
 import io
+import ssl
 from typing import IO, List, Optional
 
 from lyra.core.config import settings
+
+
+class ImplicitFTP_TLS(ftplib.FTP_TLS):
+    """FTP_TLS subclass that automatically wraps sockets in SSL to support implicit FTPS."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._sock = None
+
+    @property
+    def sock(self):
+        """Return the socket."""
+        return self._sock
+
+    @sock.setter
+    def sock(self, value):
+        """When modifying the socket, ensure that it is ssl wrapped."""
+        if value is not None and not isinstance(value, ssl.SSLSocket):
+            value = self.context.wrap_socket(value)
+        self._sock = value
 
 
 def ftp(location, username, password, directory=None):
@@ -21,8 +42,10 @@ def ftp(location, username, password, directory=None):
         ftp.retrbinary("RETR "+ "filename_src", file.write)
 
     """
-    conn = ftplib.FTP(location)
+    conn = ImplicitFTP_TLS()
+    conn.connect(location, port=990)
     conn.login(username, password)
+    conn.prot_p()
     if directory is not None:  # pragma: no branch
         conn.cwd(directory)
     return conn
